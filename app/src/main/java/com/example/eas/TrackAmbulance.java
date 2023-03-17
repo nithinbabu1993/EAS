d08552f5e78e97a157892be8112bab93d06c2e53
nithinsmac <nithin@applab.qa>
2023-03-17 10:31:43 +0530

    live tracking updated

--- app/src/main/java/com/example/eas/TrackAmbulance.java
+++ app/src/main/java/com/example/eas/TrackAmbulance.java
@@ -10,12 +10,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.eas.Dashboard.UserDashBoard;
+import com.example.eas.model.Bookingmodel;
 import com.example.eas.model.Hospitalmodel;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -36,24 +38,29 @@ import com.google.firebase.firestore.QuerySnapshot;
 public class TrackAmbulance extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-private ActivityTrackAmbulanceBinding binding;
-Bundle bid;
+    private ActivityTrackAmbulanceBinding binding;
+    Bundle bid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-     binding = ActivityTrackAmbulanceBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        binding = ActivityTrackAmbulanceBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        bid=getIntent().getExtras();
+        bid = getIntent().getExtras();
         binding.floatingMenu1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-showData();
+                LatLng latLng = new LatLng(parseDouble(Bookingmodel.latitude),
+                        parseDouble(Bookingmodel.longitude));
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(18).build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                showData();
             }
         });
     }
@@ -70,8 +77,9 @@ showData();
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        showData();
     }
+
     private void showData() {
 
         //Log.d("@", "showData: Called")
@@ -80,36 +88,30 @@ showData();
         db.collection("Booking").document(bid.getString("bid")).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        try{
-                if (documentSnapshot.getString("dlatitude") != null && documentSnapshot.getString("dlongitude") != null) {
+                try {
+                    if (documentSnapshot.getString("dlatitude") != null && documentSnapshot.getString("dlongitude") != null) {
 
-                    LatLng latLng = new LatLng(parseDouble(documentSnapshot.getString("dlatitude")),
-                            parseDouble(documentSnapshot.getString("dlongitude")));
-                    CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(12).build();
-                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                    //  mMap.clear();
-                    mMap.addMarker(new MarkerOptions()
-                            .position(latLng)
-                                    .title("Ambulance is here")
-                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))).showInfoWindow();
+                        LatLng latLng = new LatLng(parseDouble(documentSnapshot.getString("dlatitude")),
+                                parseDouble(documentSnapshot.getString("dlongitude")));
+                        CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(12).build();
+                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                        //  mMap.clear();
+                        mMap.addMarker(new MarkerOptions()
+                                .position(latLng)
+                                .title("Ambulance is here")
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))).showInfoWindow();
+                                        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                                            public void onInfoWindowClick(Marker marker) {
+                                                String issue = "http://maps.google.com/maps?q=loc:" + documentSnapshot.getString("dlatitude") + "," + documentSnapshot.getString("dlongitude") + " (Ambulance)";
 
-//                                        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
-//                                            public void onInfoWindowClick(Marker marker) {
-//                                                SharedPreferences sd = getSharedPreferences("hospital", Context.MODE_PRIVATE);
-//                                                SharedPreferences.Editor ed = sd.edit();
-//                                                ed.putString("hname", marker.getTitle());
-//                                                ed.putString("from","home");
-//                                                ed.commit();
-//                                                startActivity(new Intent(getApplicationContext(), ShowAmbulance.class));
-//                                                finish();
-//                                            }
-                    // });
-                }else{
-                    Toast.makeText(TrackAmbulance.this, "Ambulance location not found", Toast.LENGTH_SHORT).show();
+                                                locateLocation(issue);
+                                            }});
+                    } else {
+                        Toast.makeText(TrackAmbulance.this, "Ambulance location not found", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (Exception e) {
                 }
-
-        } catch(Exception e){
-        }
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -120,9 +122,15 @@ showData();
 
     }
 //
+private void locateLocation(String issue) {
+    Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(issue));
+    intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+   startActivity(intent);
+}
 
     @Override
     public void onBackPressed() {
-        startActivity(new Intent(getApplicationContext(), UserDashBoard.class));finish();
+        startActivity(new Intent(getApplicationContext(), UserDashBoard.class));
+        finish();
     }
 }
